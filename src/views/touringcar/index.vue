b33ea426cbd552f97fcefc53bb6a4d62e24dd8b2
Li Jingke 李静科 (690) <lijingke@haier.com>
2020-08-31 16:56:19 +0800

    test

--- src/views/touringcar/index.vue
+++ src/views/touringcar/index.vue
@@ -1,6 +1,5 @@
 <template>
   <el-row :gutter="20">
-      <!-- <el-button @click="btnClick">点击按钮</el-button> -->
       <el-col :span="24">
         <el-select v-model="value" placeholder="请选择" size="small" class="select">
           <el-option
@@ -92,20 +91,19 @@
 </template>
 
 <script>
-// https://blog.csdn.net/m0_38134431/article/details/99855776
+import { operationList } from '@/api/operation'
 export default {
 	data() {
 		return {
-      websock: null,
       options: [{
           value: '选项1',
-          label: '江铃防疫车'
+          label: '荣成拖挂房车A'
         }, {
           value: '选项2',
-          label: '荣成方舱'
+          label: '荣成拖挂房车B'
         }, {
           value: '选项3',
-          label: '洛阳自行式房车'
+          label: '荣成自行房车A'
         }],
         value: '选项1',
         tableData: [
@@ -114,7 +112,7 @@ export default {
             equipmentName: '顶灯',
             resource: '小程序：0，实体按键1，房车网关：2',
             dayTotalTime: '10:00:00',
-            dayAvarageTime: '15',
+            dayAvarageTime: '8:00:00',
             dayCounts: '20'
           },
           {
@@ -122,7 +120,7 @@ export default {
             equipmentName: '筒灯',
             resource: '小程序：8，实体按键15，房车网关：2',
             dayTotalTime: '8:00:00',
-            dayAvarageTime: '18',
+            dayAvarageTime: '8:00:00',
             dayCounts: '25'
           },
           {
@@ -130,15 +128,15 @@ export default {
             equipmentName: '灯带',
             resource: '小程序：3，实体按键6，房车网关：0',
             dayTotalTime: '3:00:00',
-            dayAvarageTime: '6',
+            dayAvarageTime: '8:00:00',
             dayCounts: '9'
           },
           {
-            room:'厨房',
+            room:'主卧',
             equipmentName: '气氛灯',
             resource: '小程序：3，实体按键6，房车网关：0',
             dayTotalTime: '3:00:00',
-            dayAvarageTime: '6',
+            dayAvarageTime: '8:00:00',
             dayCounts: '9'
           },
           {
@@ -146,15 +144,15 @@ export default {
             equipmentName: '顶灯',
             resource: '小程序：3，实体按键6，房车网关：0',
             dayTotalTime: '3:00:00',
-            dayAvarageTime: '6',
+            dayAvarageTime: '8:00:00',
             dayCounts: '9'
           },
           {
-            room:'次卧',
+            room:'客厅',
             equipmentName: '顶灯',
             resource: '小程序：3，实体按键6，房车网关：0',
             dayTotalTime: '3:00:00',
-            dayAvarageTime: '6',
+            dayAvarageTime: '8:00:00',
             dayCounts: '9'
           }
         ],
@@ -164,7 +162,7 @@ export default {
             modeName: '温馨',
             resource: '小程序：0，实体按键1，房车网关：2',
             dayTotalTime: '10:00:00',
-            dayAvarageTime: '15',
+            dayAvarageTime: '8:00:00',
             dayCounts: '20'
           },
           {
@@ -172,97 +170,54 @@ export default {
             modeName: '起夜',
             resource: '小程序：8，实体按键15，房车网关：2',
             dayTotalTime: '8:00:00',
-            dayAvarageTime: '18',
-            dayCounts: '25'
+            dayAvarageTime: '8:00:00',
+            dayCounts: '10'
           },
           {
             room:'主卧',
             modeName: '观影',
             resource: '小程序：3，实体按键6，房车网关：0',
             dayTotalTime: '3:00:00',
-            dayAvarageTime: '6',
-            dayCounts: '9'
+            dayAvarageTime: '8:00:00',
+            dayCounts: '8'
           },
           {
-            room:'厨房',
+            room:'客厅',
             modeName: '明亮',
             resource: '小程序：3，实体按键6，房车网关：0',
             dayTotalTime: '3:00:00',
-            dayAvarageTime: '6',
-            dayCounts: '9'
+            dayAvarageTime: '8:00:00',
+            dayCounts: '10'
           },
           {
-            room:'卫生间',
+            room:'客厅',
             modeName: '晨起',
             resource: '小程序：3，实体按键6，房车网关：0',
             dayTotalTime: '3:00:00',
-            dayAvarageTime: '6',
-            dayCounts: '9'
+            dayAvarageTime: '8:00:00',
+            dayCounts: '3'
           },
-        ]
+        ],
+        list: []
 		}
-	},
-	destroyed () {
-	    //页面销毁时关闭ws连接
-	    if(this.websock){
-	    	this.websock.close() // 关闭websocket
-	    } 
-  	},
+  },
+  watch: {
+    list(val) {
+      // console.log('watch', val)
+      console.log(val)
+    }
+  },
+  mounted() {
+    this.getData()
+  },
  	methods: {
-	    //初始化weosocket
-	    initWebSocket(){
-	      if(typeof(WebSocket) === "undefined"){
-	        alert("您的浏览器不支持WebSocket")
-	        return false
-	      }
-	      const wsuri = 'ws://*.*.*.*:8081/dns-monitor' // websocket地址
-	      this.websock = new WebSocket(wsuri)
-	      this.websock.onopen = this.websocketonopen
-	      this.websock.onmessage = this.websocketonmessage
-	      this.websock.onerror = this.websocketonerror
-	      this.websock.onclose = this.websocketclose
-	    },
-	    //连接成功
-	    websocketonopen(){ 
-	      console.log('WebSocket连接成功')
-	    },
-	    //接收后端返回的数据
-	    websocketonmessage(e){ 
-	      let dataJson = JSON.parse(e.data)
-	      console.log(dataJson)
-	      // 在这里使用后端返回的数据，对数据进行处理渲染
-	    },
-	    //连接建立失败重连
-	    websocketonerror(e){
-	      console.log(`连接失败的信息：`, e)
-	      this.initWebSocket() // 连接失败后尝试重新连接
-	    },
-	    //关闭连接
-	    websocketclose(e){ 
-	      console.log('断开连接',e)
-	    },
-	    // 点击按钮
-	  	btnClick () {
-	  	    // 因为需求是每次点击都是发起不同ws连接，拿到的数据也不一致，所以连接前要先断开上次的连接
-	  		if(this.websock){
-	       		this.websock.close() // 关闭websocket连接
-	      	}      
-	        this.initWebSocket() // 初始化weosocket，发起连接
-	        // 这里使用定时器是为了等待连接后才发送数据，避免错误
-	        setTimeout( () => { 
-	           //添加状态判断，当为OPEN时，前端发送消息给后端
-	           if (this.websock.readyState === 1) {
-	           		// 把后台需要的参数数据传过去
-	               let obj = {
-		               jobId: 111,
-		               token: 'xxxxx',
-		               message: 'send ok'
-	              }
-	         	 //发给后端的数据需要字符串化
-	         	 this.websock.send(JSON.stringify(obj))
-	       		 }
-	      	}, 500)
-	  	}
+     //根据房车-房间获取设备、模式
+     getData() {
+       setInterval(async () => { 
+          this.list = await operationList()
+          // console.log('list', this.list)
+        }, 10000)
+     }
 	  }
 }
 </script>
